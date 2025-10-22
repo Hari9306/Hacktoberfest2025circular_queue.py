dc549fca55feafce0212d26bb5cefccd0c6716cf
Hari9306 <hariveera9306@gmail.com>
2025-10-22 12:49:32 +0530

    Create circular_queue.py

---
+++ circular_queue.py
@@ -0,0 +1,30 @@
+class CircularQueue:
+    def __init__(self, size):
+        self.size = size
+        self.queue = [None] * size
+        self.front = self.rear = -1
+
+    def enqueue(self, item):
+        if (self.rear + 1) % self.size == self.front:
+            return "Full"
+        elif self.front == -1:
+            self.front = self.rear = 0
+        else:
+            self.rear = (self.rear + 1) % self.size
+        self.queue[self.rear] = item
+
+    def dequeue(self):
+        if self.front == -1:
+            return "Empty"
+        item = self.queue[self.front]
+        if self.front == self.rear:
+            self.front = self.rear = -1
+        else:
+            self.front = (self.front + 1) % self.size
+        return item
+
+cq = CircularQueue(3)
+cq.enqueue(1)
+cq.enqueue(2)
+cq.enqueue(3)
+print(cq.enqueue(4))  # Expected "Full", but ❌ returns None
